2f65afc10663801526447acf19b96b7d2948c417
Vinod Chandra <vinodchandra.net@gmail.com>
2018-09-21 14:58:45 -0400

    Added StatusBar

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { Constants } from 'expo'
 import { AddDeck } from './components/decks/AddDeck'
 import { ViewDecks } from './components/decks/ViewDecks'
 import { AddCard } from './components/cards/AddCard'
 import { ViewCard } from './components/cards/ViewCard'
 import { Quiz } from './components/cards/Quiz'
 
+function FlashCardsStatusBar() {
+  return (
+    <View style={{ height: Constants.statusBarHeight }}>
+      <StatusBar translucent barStyle="light-content" />
+    </View>
+  )
+}
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Quiz />
+      <View style={{ flex: 1 }}>
+        <FlashCardsStatusBar />
+        <View
+          style={{
+            flex: 1,
+          }}>
+          <Quiz />
+        </View>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
