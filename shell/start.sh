f0e27311dd5c470e69e2b41e82770ae2d7b39ec2
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-09-25 20:50:36 -0400

    react-native-debugger commands

--- shell/start.sh
+++ shell/start.sh
@@ -1,3 +1,7 @@
 export REACT_DEBUGGER="rndebugger-open --open --port 19001" &&
+# enable below command
+# ====================
+# for Mac only and without react-native-debugger-open package
+#export REACT_DEBUGGER="unset ELECTRON_RUN_AS_NODE && open -g 'rndebugger://set-debugger-loc?port=19001'" &&
 watchman watch-del-all &&
 yarn start --reset-cache
--- shell/startX.sh
+++
@@ -1,3 +0,0 @@
-export REACT_DEBUGGER="unset ELECTRON_RUN_AS_NODE && open -g 'rndebugger://set-debugger-loc?port=19001'" &&
-watchman watch-del-all &&
-yarn start --reset-cache
