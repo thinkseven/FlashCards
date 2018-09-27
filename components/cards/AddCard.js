9fb84c830e99267a073168476d220e76fc7b8c4b
VINOD CHANDRA <vinodchandra.net@gmail.com>
2018-09-27 07:48:52 -0400

    Add Card Component changes

--- components/cards/AddCard.js
+++ components/cards/AddCard.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { HandleAddCard } from '../../actions'
 
-export default class AddCard extends Component {
+class AddCard extends Component {
+  state = {
+    question: '',
+    answer: '',
+  }
+
+  handleQuestionInput = text => {
+    this.setState({
+      question: text,
+    })
+  }
+
+  handleAnswerInput = text => {
+    this.setState({
+      answer: text,
+    })
+  }
+
+  submitCard = () => {
+    const { dispatch, navigation } = this.props
+    const title = navigation.getParam('title')
+    const { question, answer } = this.state
+    dispatch(
+      HandleAddCard(title, {
+        question,
+        answer,
+      }),
+    )
+    this.setState({
+      question: '',
+      answer: '',
+    })
+  }
+
   render() {
     return (
       <View>
-        <Text>AddCard Component!!</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Deck Title"
+          onChangeText={this.handleQuestionInput}
+          value={this.state.question}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Deck Title"
+          onChangeText={this.handleAnswerInput}
+          value={this.state.answer}
+        />
+        <TouchableOpacity onPress={this.submitCard}>
+          <View>
+            <Text>Submit</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+export default connect()(AddCard)
